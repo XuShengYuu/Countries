81c5dd04dd8abc3420877a66f7b472454ea429a7
hello <45997837+XuShengYuu@users.noreply.github.com>
2021-07-02 21:31:35 +0800

    finish header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <div class="header-title">Where in the world?</div>
+    <div class="header-butter">
+      <span class="iconfont">&#xe7d3;</span>
+      Dark Mode</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeHeader',
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    line-height: 75px
+    height: 75px
+    box-shadow: 0px 2px 5px #ccc;
+    .header-title
+      float: left
+      margin-left: 65px
+      font-size: 25px
+      font-weight: bold
+    .header-butter
+      float: right
+      margin-right: 65px
+      font-size: 600
+</style>
