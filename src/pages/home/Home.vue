ba2da48404f68a5965cdae75369f975252412a73
hello <45997837+XuShengYuu@users.noreply.github.com>
2021-07-02 21:55:58 +0800

    add index-search

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,25 @@
 <template>
-  <home-header></home-header>
+  <div>
+    <home-header></home-header>
+    <div class="content">
+      <home-search></home-search>
+    </div>
+  </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSearch from './components/Search'
 export default {
     name: 'Home',
     components: {
-      HomeHeader
+      HomeHeader,
+      HomeSearch
     }
   }
 </script>
-<style>
-  
+<style lang="stylus">
+.content
+  width: 1440px
+  margin: 0 auto;
 </style>
---
+++ src/pages/home/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search">
+    <span class="iconfont">
+      &#xe725;
+      <input class="search-input" results="s" type="text" placeholder="Search for a country……">
+    </span>    
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSearch',
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .search
+    height:45px
+    line-height:45px
+    width: 400px
+    margin-top: 40px
+    border-radius: 5px
+    box-shadow: 0px 0px 10px #ccc;
+    .iconfont
+      margin-left: 20px
+      .search-input
+        margin-left: 10px
+        line-height:35px
+        border: 0px
+</style>
